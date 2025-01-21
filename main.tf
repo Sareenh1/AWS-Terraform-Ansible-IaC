90adf2ac67f32f7a2b35377c5fc4a76e0b9976fe
H Sareen Kumar <sareenh10@gmail.com>
2025-01-21 19:38:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,10 @@
      ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (update as needed)
      instance_type = "t2.micro"
      subnet_id     = aws_subnet.subnet.id
+     security_group = aws_security_group.allow_ssh.id
+     key_name      = aws_key_pair.key_pair.key_name
+
+     tags = {
+       Name = "MyInstance"
+     }
+   }
